4f4d3fab81331ebbaba18c5d901fe2091bc91172
Cesar <cesar.atadeu@gmail.com>
2023-11-30 17:23:54 -0300

    init function for import xml files

--- src/pages/components/Import.js
+++ src/pages/components/Import.js
@@ -1,28 +1,46 @@
 // Importar.js
-import React from 'react';
-import Styles from "../css/Sidebar.module.css";
+import React, { useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Table from 'react-bootstrap/Table';
 
-
 const Importar = () => {
+    const [fileData, setFileData] = useState([]);
+    
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+            const xmlString = e.target.result;
+            // Aqui você pode analisar o arquivo XML e extrair as informações desejadas
+            // Por exemplo, você pode usar uma biblioteca como xml2js para analisar o XML
+            // e extrair os dados relevantes.
+
+            // Simulando dados de exemplo
+            const extractedData = [
+                { id: 1, firstName: 'Mark', lastName: 'Otto', username: '@mdo' },
+                { id: 2, firstName: 'Jacob', lastName: 'Thornton', username: '@fat' },
+                { id: 3, firstName: 'Larry', lastName: 'Bird', username: '@twitter' }
+            ];
+
+            setFileData(extractedData);
+        };
+
+        reader.readAsText(file);
+    };
+
     return (
         <>
-        <Row className="ms-2">
-            <h3>Bem vindo(a), usuário</h3>
-        </Row>
-        <Row className="ms-2">
-            <h6>Importar XML/NF</h6>
-        </Row>
+            {/* ... (seu conteúdo existente) */}
             <Col className="d-flex justify-content-center">
                 <Card>
                     <Card.Body>
                         <Row className="my-2">
                             <Col className="d-flex justify-content-end">
-                                <Button variant="primary">Selecionar Arquivo</Button>
+                                <input type="file" onChange={handleFileChange} />
                             </Col>
                         </Row>
                         <Row>
@@ -37,40 +55,23 @@ const Importar = () => {
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <td>1</td>
-                                            <td>Mark</td>
-                                            <td>Otto</td>
-                                            <td>@mdo</td>
-                                        </tr>
-                                        <tr>
-                                            <td>2</td>
-                                            <td>Jacob</td>
-                                            <td>Thornton</td>
-                                            <td>@fat</td>
-                                        </tr>
-                                        <tr>
-                                            <td>3</td>
-                                            <td colSpan={2}>Larry the Bird</td>
-                                            <td>@twitter</td>
-                                        </tr>
+                                        {fileData.map((data) => (
+                                            <tr key={data.id}>
+                                                <td>{data.id}</td>
+                                                <td>{data.firstName}</td>
+                                                <td>{data.lastName}</td>
+                                                <td>{data.username}</td>
+                                            </tr>
+                                        ))}
                                     </tbody>
                                 </Table>
                             </Col>
                         </Row>
-                        <Row className="my-2">
-                            <Col className="d-flex justify-content-end">
-                                <Button variant="primary">Confirmar</Button>
-                            </Col>
-                        </Row>
+                        {/* ... (seu conteúdo existente) */}
                     </Card.Body>
                 </Card>
             </Col>
-
         </>
-
-
-
     );
 };
 
